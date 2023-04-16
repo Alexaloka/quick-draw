30eab0da99b28f5f4ca88a4ad59ea1eaac20b0a8
Alexaloka <103824973+Alexaloka@users.noreply.github.com>
2023-04-16 16:48:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-var quick_draw =[butterfly,heart,rose,watermelon,lipstick,hockey,phone,cake,car,rainbow];
+var quick_draw =["butterfly","heart","rose","watermelon","lipstick","hockey","phone","cake","car","rainbow"];
 answer_holder = "";
 drawn_sketch = "";
 score = 0;
-timer_counter = 0;
+time_counter = 0;
 timer_check = "" ;
-random_no = Math.floor((Math.random()*quick_draw)+1);
+
+random_no = Math.floor((Math.random()*quick_draw.length)+1);
 elements = quick_draw[random_no];
-document.getElementById("ogsketch").innerHTML = "Sketch to be drawn :"+ elements;
+document.getElementById("ogSketch").innerHTML = "Sketch to be drawn :"+elements;
 function setup()
 {
     canvas = createCanvas(280,280);
@@ -28,20 +29,32 @@ document.getElementById("score").innerHTML = "Score : " + score;
 }
 function check_sketch()
 {
-timer_counter = timer_counter + 1;
-console.log(timer_counter);
-document.getElementById("timer").innerHTML = "Time : " + timer_counter;
-if (timer_counter > 400)
+time_counter = time_counter + 1;
+console.log(time_counter);
+document.getElementById("time").innerHTML = "Timer : " + time_counter;
+if (time_counter > 400)
 {
-    timer_check = "compleated";
-timer_counter = 0;
+    time_check = "compleated";
+time_counter = 0;
 
 }
 if(timer_check == "compleated"||answer_holder == "set")
 {
-timer_check = "";
+time_check = "";
 answer_holder = "";
 updateCanvas();
 
 }
 }
+function updateCanvas()
+{
+    background("white");
+    random_no = Math.floor((Math.random()*quick_draw.length)+1);
+    console.log(quick_draw[random_no]);
+    elements = quick_draw[random_no];
+    document.getElementById("ogsketch").innerHTML = "Sketch to be drawn :"+ elements;
+
+
+
+}
+
